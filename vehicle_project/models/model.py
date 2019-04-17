63a8fee3151e5053e22623fd343efbdbf6538be1
Kashif Aziz <kashifaziz123@hotmail.com>
2019-04-17 23:02:52 +0500

    company_id issue

--- vehicle_project/models/model.py
+++ vehicle_project/models/model.py
@@ -232,7 +232,7 @@ class subtaskcomponent(models.Model):
         for line in self:
             fpos = sale.fiscal_position_id or sale.partner_id.property_account_position_id
             # If company_id is set, always filter taxes by the company
-            taxes = line.product_id.taxes_id.filtered(lambda r: not line.company_id or r.company_id == line.company_id)
+            taxes = line.product_id.taxes_id.filtered(lambda r: not self.env.user.company_id or r.company_id == self.env.user.company_id)
             line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
 
     @api.one
